27653724a7f7d7ae795273bbac35918687f31d7d
eNJi25 <nfischer.contact@gmail.com>
2025-10-27 21:44:09 +0100

    Ajoute système de routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,7 @@
+import Route from "./Route.js";
+
+// Définir les routes ici
+export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
+
+// Le titre de la page = le nom de la page en cours
+export const websiteName = "Quai Antique";
