c81d7aea5c8c9fcbf4bb2f36be64f520014000a6
eNJi25 <nfischer.contact@gmail.com>
2025-11-16 14:22:33 +0100

    Ajout de la page Galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,7 +1,10 @@
 import Route from "./Route.js";
 
 // Définir les routes ici
-export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/galerie", "Galerie", "/pages/galerie.html"),
+];
 
 // Le titre de la page = le nom de la page en cours
 export const websiteName = "Quai Antique";
